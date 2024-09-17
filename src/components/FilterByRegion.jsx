b3c816c126322f87885f153fa4da519a1283376d
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-09-17 08:13:37 +0530

    console statement removed

--- src/components/FilterByRegion.jsx
+++ src/components/FilterByRegion.jsx
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from "react";
 const FilterByRegion = ({ data, setCountriesData }) => {
-  console.log(data, setCountriesData);
-
   const [regionValue, setRegionValue] = useState("");
-
   useEffect(() => {
     const filterInfo = () => {
       let filterData = [];
