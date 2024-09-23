67ff04ba1aefaba8367dedf33062f4cb96f49ffc
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2024-09-23 08:55:02 +0530

    border countries added using promise.all

--- src/components/CountryInfo.jsx
+++ src/components/CountryInfo.jsx
@@ -1,26 +1,41 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import Shimmer from "./Shimmer";
 
 const CountryInfo = () => {
   const { name } = useParams();
   const [countryData, setCountryData] = useState([]);
+  const [borderCountries, setBorderCountries] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchCountry = async () => {
       try {
         const response = await fetch(
-          `https://restcountries.com/v3.1/name/${name}`
+          `https://restcountries.com/v3.1/name/${name}?fullText=true`
         );
         const data = await response.json();
         setCountryData(data);
         setLoading(false);
+
+        // Fetch border countries if available
+        if (data[0].borders && data[0].borders.length > 0) {
+          const borders = data[0].borders;
+          const borderNames = await Promise.all(
+            borders.map((border) =>
+              fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+                .then((response) => response.json())
+                .then(([countryData]) => countryData.name.common)
+            )
+          );
+          setBorderCountries(borderNames);
+        }
       } catch (error) {
         console.error("Error fetching country data:", error);
         setLoading(false);
       }
     };
+
     fetchCountry();
   }, [name]);
 
@@ -78,9 +93,18 @@ const CountryInfo = () => {
             ? Object.values(country.languages).join(", ")
             : "No Languages Available"}
         </h4>
+
         <h4>
-          <span>Border Countries:</span>{" "}
-          {country.borders ? country.borders.join(", ") : "No Border Countries"}
+          <span>Border Countries:</span>
+          <div className="border-countries">
+            {borderCountries.length > 0
+              ? borderCountries.map((border, index) => (
+                  <Link key={index} to={`/name/${border}`}>
+                    {border}
+                  </Link>
+                ))
+              : "No Border Countries"}
+          </div>
         </h4>
       </div>
     </div>
